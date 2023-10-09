4c054daa55178958ccb3fa665b55d5f581cf410f
pravarmishra <131448543+pravarmishra@users.noreply.github.com>
2023-10-09 13:35:02 +0530

    Cahnges

--- src/modules/database/database.js
+++ src/modules/database/database.js
@@ -2,6 +2,7 @@ import * as Parse from "parse";
 
 Parse.initialize("9Ov2ElZ2ecmuJF3ptlUdgMLxiiL");
 Parse.serverURL ="https://iil-dev-64e66426455a.herokuapp.com/parse/";
+
 // console.log("reading env",process.env.REACT_APP_SERVER_URL )
 const logOut = async () => {
     try {
